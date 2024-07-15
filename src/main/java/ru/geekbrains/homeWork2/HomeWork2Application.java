6669a2380c75e85b6b4c4690c270c864bf1b7817
arew <misteac13@gmail.com>
2024-07-15 17:53:21 +0300

    init commit

---
+++ src/main/java/ru/geekbrains/homeWork2/HomeWork2Application.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.homeWork2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeWork2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeWork2Application.class, args);
+	}
+
+}
---
+++ src/test/java/ru/geekbrains/homeWork2/HomeWork2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.geekbrains.homeWork2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeWork2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
